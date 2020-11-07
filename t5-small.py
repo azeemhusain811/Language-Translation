d4254e77a3d10e0bb244f1d02e35ffd4ae1127c2
Azeem Hussain <32011670+azeemhusain811@users.noreply.github.com>
2020-11-08 02:56:27 +0530

    Added t5-small.py

---
+++ t5-small.py
@@ -0,0 +1,16 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained('t5-small')
+
+model = T5ForConditionalGeneration.from_pretrained('t5-small', return_dict=True)
+
+input = "My name is Wolfgang and I live in Berlin"
+
+# We can also use "translate English to French" and "translate English to Romanian"
+input_ids = tokenizer("translate English to German: "+input, return_tensors="pt").input_ids  # Batch size 1
+
+outputs = model.generate(input_ids)
+
+decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+print(decoded)
